1952973c33b074b4c56fb2d77d08225d8cf8465a
Promtada kuniart <6810110220@psu.ac.th>
2025-12-29 21:07:00 +0700

    Setup User Entity

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,24 +1,22 @@
+// server/src/app.module.ts
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './users/user.entity'; // 1. นำเข้าไฟล์ User
 
 @Module({
   imports: [
-    // 1. โหลดค่าจาก .env
     ConfigModule.forRoot(),
-    
-    // 2. ตั้งค่าการเชื่อมต่อ Database
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DATABASE_HOST,
-      // แก้ตรงนี้: ถ้าไม่มีค่าให้อ่านเป็น '5432' แทน
       port: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
-      entities: [],
+      entities: [User], // 2. ใส่ User ลงในนี้ เพื่อให้ Database รู้จัก
       synchronize: true, 
     }),
   ],
